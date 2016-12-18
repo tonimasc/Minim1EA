795df692840fd2fbf754893cdefee78d5e943fd2
tonimsc <tonimxc@gmail.com>
2016-12-18 15:53:28 +0100

    cors filter added

--- app.js
+++ app.js
@@ -22,6 +22,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//CORS header
+app.all('/*', function(req, res, next) {
+    // CORS headers
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Access-Control-Allow-Origin');
+    res.header("Access-Control-Max-Age", "86400"); // 24 hours
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
+
 app.use('/subjects', subjects);
 app.use('/students', students);
 
